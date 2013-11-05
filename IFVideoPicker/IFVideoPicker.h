8a2b7fb0ac17af894152632c1dc110302bbef299
minsikzzang <minsikzzang@gmail.com>
2013-11-05 14:07:07 +0000

    Initial Commit

---
+++ IFVideoPicker/IFVideoPicker.h
@@ -0,0 +1,13 @@
+//
+//  IFVideoPicker.h
+//  IFVideoPicker
+//
+//  Created by Min Kim on 11/5/13.
+//  Copyright (c) 2013 iFactory Lab Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IFVideoPicker : NSObject
+
+@end
---
+++ IFVideoPicker/IFVideoPicker.m
@@ -0,0 +1,13 @@
+//
+//  IFVideoPicker.m
+//  IFVideoPicker
+//
+//  Created by Min Kim on 11/5/13.
+//  Copyright (c) 2013 iFactory Lab Limited. All rights reserved.
+//
+
+#import "IFVideoPicker.h"
+
+@implementation IFVideoPicker
+
+@end
